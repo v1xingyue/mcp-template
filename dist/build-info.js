35cfd393f0d2531448dd205f1bf9a42aefde70fc
v1xingyue <qixingyue@gmail.com>
2025-03-23 18:49:56 +0800

    version 22

--- dist/build-info.js
+++ dist/build-info.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUILD_TIME = void 0;
-exports.BUILD_TIME = '2025-03-23 18:48:56';
+exports.BUILD_TIME = '2025-03-23 18:49:48';
--- src/build-info.ts
+++ src/build-info.ts
@@ -1,1 +1,1 @@
-export const BUILD_TIME = '2025-03-23 18:48:56';
+export const BUILD_TIME = '2025-03-23 18:49:48';
