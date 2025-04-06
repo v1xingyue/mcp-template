b7b60faf1af73cf18bb10100209c732736626fcb
v1xingyue <qixingyue@gmail.com>
2025-04-06 09:37:38 +0800

    add tag

--- dist/build-info.js
+++ dist/build-info.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.BUILD_TIME = void 0;
-exports.BUILD_TIME = '2025-04-06 09:36:20';
+exports.BUILD_TIME = '2025-04-06 09:37:29';
--- src/build-info.ts
+++ src/build-info.ts
@@ -1,1 +1,1 @@
-export const BUILD_TIME = '2025-04-06 09:36:20';
+export const BUILD_TIME = '2025-04-06 09:37:29';
